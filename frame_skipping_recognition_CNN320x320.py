c6c9aaaa432c3e3da23bda60593be1db4eeaf690
wpolyn <wojciech.polyn@gmail.com>
2025-08-12 23:42:47 +0200

    resolution changes commit

--- frame_skipping_recognition_CNN320x320.py
+++ frame_skipping_recognition_CNN320x320.py
@@ -1,6 +1,6 @@
 import cv2
 
-from inits2 import initialize_camera, text_properties
+from inits2 import initialize_camera, text_properties #640x480
 input_size = (320, 320)
 display_size = (320, 240)
 crop_size = 320
@@ -24,13 +24,13 @@ if __name__ == '__main__':
         key = cv2.waitKey(1) & 0xFF
         if ret:
             #model input processing
-            h, w = image.shape[:2]
-            x_input = (w - crop_size) // 2
-            y_input = (h - crop_size) // 2
-            image = image[y_input:y_input+crop_size, x_input:x_input+crop_size]
-            #image = cv2.flip(image,1)
-            image = cv2.resize(image, input_size)
-            #image = cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE)
+            h, w = image.shape[:2] # h=480 w=640
+            x_input = (w - crop_size) // 2 # =160
+            y_input = (h - crop_size) // 2 # =80
+            image = cv2.flip(image,1)
+            image = image[y_input:y_input+crop_size, x_input:x_input+crop_size] #y: 80:400 x: 160:480 -> 320x320
+            image = cv2.resize(image, input_size) #w razie czego
+            #image = cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE) #x=320 <-> y=320, x->240 for display
             retval, faces = detector.detect(image)
 
             if frame_counter % frame_buffer == 0:
@@ -50,10 +50,13 @@ if __name__ == '__main__':
             else:
                 frame_counter += 1
             #display processing
-            x_display = 0        
-            y_display = (display_size[0] - display_size[1]) // 2
-            display = image[y_display:y_display+display_size[1],x_display:display_size[0]]
-            #display = cv2.flip(display,1)
+            #x_display = 0
+            y_display = 0        
+            x_display = (display_size[0] - display_size[1]) // 2 #(320-240)/2 = 40
+            #display = image[y_display:y_display+display_size[1],x_display:display_size[0]] # y 40:280 x 0:320
+            #display = image[x_display:display_size[0], y_display:y_display+display_size[1]] # x 40:280 y 0:320
+            display = image[y_display:y_display+display_size[0],x_display:x_display+display_size[1]] # x 40:280 y 0:320
+            
             #display = cv2.rotate(display, cv2.ROTATE_90_COUNTERCLOCKWISE)
             for face, recognition in previous_face:
                 x, y, w, h = map(int, face[:4])
